cec7b059dcd06aea8c90881c8a39a9448f342c86
eeric <eeric@users.noreply.github.com>
2021-02-25 14:19:58 +0800

    Update __init__.py

--- model-arch/__init__.py
+++ model-arch/__init__.py
@@ -7,7 +7,6 @@ def build_model(model_cfg):
         model = GFL(model_cfg.arch.backbone, model_cfg.arch.fpn, model_cfg.arch.head)
     elif model_cfg.arch.name == 'GFL_t':
         model = GFL_t(model_cfg.arch.backbone, model_cfg.arch.fpn, model_cfg.arch.head)
-
     else:
         raise NotImplementedError
     return model
